34976f5c0e7d08179a6a31b9bc37106af4bb6e0b
poorvaditya18 <aditya.behre@gmail.com>
2022-12-17 19:11:30 +0530

    completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,19 +1,28 @@
 // Repository will help us to interact with the models(databases)
+
+// Reponsible for communicating with datasource it can either be from database or any other source
+
+// we dont want tight coupling therefore we make this layer .
+
 const { City } = require("../models/index");
 
 class CityRepository {
-  // create city
+
+//    create city  
   async createCity({ name }) {
+    // {name: "New Delhi"}
     try {
-      // .create() -> is available in sequelize
-      const city = await City.create({ name });
+      const city = await City.create({
+        name,
+      });
       return city;
     } catch (error) {
+      console.log("Something went wrong in the repository layer");
       throw { error };
     }
   }
 
-  // delete city
+//   delete city
   async deleteCity(cityId) {
     try {
       await City.destroy({
@@ -21,10 +30,35 @@ class CityRepository {
           id: cityId,
         },
       });
+      return true;
     } catch (error) {
+      console.log("Something went wrong in the repository layer");
+      throw { error };
+    }
+  }
+
+// update city
+  async updateCity(cityId, data) {
+    try {
+      const city = await City.update(data, {
+        where: {
+          id: cityId,
+        },
+      });
+      return city;
+    } catch (error) {}
+  }
+
+//   get city
+  async getCity(cityId) {
+    try {
+      const city = await City.findByPk(cityId);
+      return city;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer");
       throw { error };
     }
   }
 }
 
-module.exports = CityRepository
+module.exports = CityRepository;
